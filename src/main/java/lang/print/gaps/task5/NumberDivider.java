4e6d91e77fb82a3a9ce7fdb89f71720ae5576c0a
Azim Shamuratov <lennon645@yandex.com>
2022-09-08 15:33:26 +0500

    Task #5. Operators

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        float dividedNumber = toBeDivided / 5f;
+        System.out.println(dividedNumber);
     }
 }
